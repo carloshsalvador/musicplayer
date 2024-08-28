7a547c2ae84bd616086eb10182ab6665182dae1d
carlos <carloshsalvador@hotmail.com>
2024-08-28 18:02:23 +0200

    settings api/views.py requires also new library djangorestframework (modulo rest_framework used on this script). requirements.txt was thus also updated

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
 # Create your views here.
+# api/views.py
+
+import rest_framework
+import djangorestframework
+
+print(rest_framework.__version__)
+
+import sys
+print(sys.path)
+
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Song
+from .serializers import SongSerializer
+
+class SongListCreateView(generics.ListCreateAPIView):
+	queryset = Song.objects.all()
+	serializer_class = SongSerializer
+
+class SongDetailView(generics.RetrieveUpdateDestroyAPIView):
+	queryset = Song.objects.all()
+	serializer_class = SongSerializer
+
