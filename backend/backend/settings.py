77de77cc9803995aae438f4404c3c593015c61ab
carlos <carloshsalvador@hotmail.com>
2024-08-29 18:35:07 +0200

    new files automatically created after 'python manage.py check --deploy', because I run it before deploy. The deploy come later, now is just check!

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9r-x&1ba$=ihclhb4+d2^h$7q-r+%sv+39ohrw&=u=i@-@hwgv'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
