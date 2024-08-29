e12d92374aa412346518ff986231809a656e6e16
carlos <carloshsalvador@hotmail.com>
2024-08-29 17:03:21 +0200

    create and update the admin.py files at api and music_player_app folder each. They are the last step before migration and deploy Django app

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,9 @@
+### api/admin.py
 from django.contrib import admin
-
-# Register your models here.
+from .models import Song
+ 
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('id','title','categorie', 'artist', )
+    search_fields = ('title', 'artist')
+ 
+admin.site.register(Song, SongAdmin)
