bed8e3cb41338cf708dae954606dfc23bbdb10c9
carlos <carloshsalvador@hotmail.com>
2024-09-03 14:21:17 +0200

    adjusts for Dockercompose in regards to the DATABANKs' variables on .env, settings.py e docker-compose.yml

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,11 +129,11 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DATABASE_NAME'),
+        'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
-        'PORT': os.getenv('POSTGRES_PORT', '5432'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
