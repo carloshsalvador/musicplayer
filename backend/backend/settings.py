2071a1b4ed13380dd481c3607c38b919395ae59c
carlos <carloshsalvador@hotmail.com>
2024-09-05 16:00:12 +0200

    changing on settings.py about DATABANK with 2 scenarios, one for develpment (using sqlight) e other for production (using postsql). This change has impolication on so other files.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -125,17 +125,30 @@ TEMPLATES = [
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+# here ajusted to use PostgreSQL instead of Djang's default SQLite
+# also set for different scenarios, development (DEBUG=False) and production (DEBUG=False)
+# for example, when DEBUG=True, the database is set to SQLite, which is a simple and easy to use database, perfect for development, and has no problem of connections and functions such manage.py mingrate.
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': '5432',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB'),
+            'USER': os.getenv('POSTGRES_USER'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+            'HOST': os.getenv('POSTGRES_HOST', 'db'),
+            'PORT': os.getenv('POSTGRES_PORT', '5432'),
+        }
+    }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
